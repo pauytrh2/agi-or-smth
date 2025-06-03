6a0b8c329374c5e8fd95f85e04e35f9658bf54fe
pauytrh2 <pauytrh@gmail.com>
2025-06-03 20:54:01 +0300

    I give up

--- speech/stt.py
+++ speech/stt.py
@@ -1,23 +1,23 @@
 import speech_recognition as sr
-from tts import speak
+from speech.tts import speak
 
 def listen():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        try:
-            audio = recognizer.listen(source, timeout=5)
-            return recognizer.recognize_google(audio)
-        except sr.WaitTimeoutError:
-            print("No speech detected within timeout")
-            return None
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-            return None
-        except sr.RequestError as e:
-            print(f"Could not request results; {e}")
-            return None
-    # return input("What is your prompt?: ")
+        # try:
+        #     audio = recognizer.listen(source, timeout=5)
+        #     return recognizer.recognize_google(audio)
+        # except sr.WaitTimeoutError:
+        #     print("No speech detected within timeout")
+        #     return None
+        # except sr.UnknownValueError:
+        #     print("Could not understand audio")
+        #     return None
+        # except sr.RequestError as e:
+        #     print(f"Could not request results; {e}")
+        #     return None
+    return input("What is your prompt?: ")
 
 if __name__ == "__main__":
     while True:
